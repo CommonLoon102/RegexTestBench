9ef7b98eb32914e341b518b85238312a93659e27
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2020-02-23 21:54:01 +0100

    Add project files.

---
+++ RegexTestBench/RegexTestBench.Wpf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace RegexTestBench.Wpf
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Wpf).Run(new MainForm());
+        }
+    }
+}
